943db5ae65b97db026ff1bc95b975707981d81bb
Ville <villesc@gmail.com>
2025-01-30 09:26:02 +0200

    BlackJack improved

--- Template/src/main/java/BlackJack.java
+++ Template/src/main/java/BlackJack.java
@@ -8,8 +8,7 @@ public class BlackJack extends Game {
     List<Card> cards;
     private Player winner;
     private Player looser;
-    Player bestPlayer;
-    int playerIndex;
+   Player  currentPlayer;
 
     public BlackJack() {
         players = new ArrayList<>();
@@ -18,10 +17,11 @@ public class BlackJack extends Game {
         droppedPlayers = new ArrayList<>();
 
     }
+
     //Ask player name and add it to game
     @Override
     public void initializeGame(int numberOfPlayers) {
-        bestPlayer = null;
+        winner = null;
         this.numberOfPlayers = numberOfPlayers;
         //if reinitializing game  add dropped players back to game
         addDroppedPlayersBack();
@@ -55,211 +55,217 @@ public class BlackJack extends Game {
     //This method is called to play single turn of player
     @Override
     public void playSingleTurn(int playerIndex) {
-        this.playerIndex = playerIndex;
-        Player player = players.get(playerIndex);
-        if (players.size() == 1) {
-            System.out.println("Only one player left, game ends");
-            System.out.println("Winner is " + player.name);
-            System.out.println(player.name + " has " + player.getValue());
-            this.winner = player;
-            return;
+        for(Player plr : players){
+            if(plr.index == playerIndex){
+                currentPlayer = plr;;
+            }
         }
+        checkIfWinner();
+        while (winner == null) {
 
-            while (true) {
-                System.out.println(player.name + " do you want to take new card Y/N or Stay(S)");
-                String answer = sc.nextLine().toUpperCase();
-                switch (answer) {
-                    case "Y":
-                        player.takeCard(cards.remove(0));
-                        printHand(player);
-                        //check if player has 21 or more
-                        if (checkIfWinner()) {
+            if (players.size() == 1) {
+                System.out.println("Only one player left, game ends");
+                System.out.println("Winner is " + currentPlayer.name);
+                System.out.println(currentPlayer.name + " has " + currentPlayer.getValue());
+                this.winner = currentPlayer;
+                return;
+            }
+            System.out.println(currentPlayer.name + " do you want to take new card Y/N or Stay(S)");
+            String answer = sc.nextLine().toUpperCase();
+            switch (answer) {
+                case "Y":
+                    currentPlayer.takeCard(cards.remove(0));
+                    printHand(currentPlayer);
+                    //check if player has 21 or more
+                    if (checkIfWinner()) {
+                        return;
+                    }
+                    for (Player droppedPlayer : droppedPlayers) {
+                        if (droppedPlayer.name.equals(currentPlayer.name)) {
                             return;
                         }
-                        for (Player droppedPlayer : droppedPlayers) {
-                            if (droppedPlayer.name.equals(player.name)) {
-                                return;
-                            }
-                        }
-                        break;
+                    }
+                    break;
 
-                    case "S":
-                        player.stay = true;
-                       return;
-                    case "N":
-                        return;
+                case "S":
+                    currentPlayer.stay = true;
+                    return;
+                case "N":
+                    return;
 
-                    default:
-                        System.out.println("Invalid input");
-                        break;
+                default:
+                    System.out.println("Invalid input");
+                    break;
 
-                }
             }
-
-
+        }
 
 
     }
     //*********************************CHECK HANDS******************************************************
 
 
-
-
-
-//This method is called to give 2 cards to players in start
-public void initialHand() {
-    for (Player player : players) {
-        if (player.getCards().isEmpty()) {
-            while (player.getCards().size() < 2) {
-                player.takeCard(cards.remove(0));
-
-
-            }
-        }
-
-        printHand(player);
-    }
-    checkIfWinner();
-}
-
-
-//This method is called to print players hand
-public void printHand(Player player) {
-    System.out.println(player.name + "  **HAND**: ");
-    List<Card> hand = player.getCards();
-    for (Card card : hand) {
-        System.out.println(card.toString());
-
-    }
-
-}
-
-public boolean checkIfWinner() {
-    Iterator<Player> iterator = players.iterator();
-    while (iterator.hasNext()) {
-        Player player = iterator.next();
-        int currentPlayerHand = player.getValue();
-
-        // if player has more than 21 he loses
-        if (currentPlayerHand > 21) {
-            droppedPlayers.add(player);
-            iterator.remove();
-            System.out.println(player.name + " has more than 21 and is OUT");
-        }
-
-        // if player has 21 it wins
-        if (currentPlayerHand == 21) {
-            this.winner = player;
-            return true;
-        }
-
-        // if all players stay check who has best hand
-        if (allPlayersStayed()) {
-            if (bestPlayer == null || bestPlayer.getValue() < currentPlayerHand) {
-                bestPlayer = player;
-            }
-            this.winner = bestPlayer;
-            return true;
-        }
-    }
-    return false;
-}
-
-public boolean allPlayersStayed() {
-    for (Player player : players) {
-        if (!player.stay) {
-            return false;
-        }
-    }
-    return true;
-}
-
-public void clearHands() {
-    for (Player player : players) {
-        player.clearHand();
-
-
-    }
-
-
-}
-
-private void addDroppedPlayersBack() {
-    for (int i = 0; i < droppedPlayers.size(); i++) {
-        if (i < droppedPlayers.size()) {
-            players.add(droppedPlayers.remove(i));
-        }
-    }
-}
-
-public void emptyPlayers() {
-    for (Player player : players) {
-        droppedPlayers.add(players.remove(player.index));
-    }
-
-
-}
-
-//This method is called to check if game has ended if so it will ask if player wants to play again or end game
-@Override
-public boolean endOfGame() {
-    if (this.winner != null) {
-        this.winner.wins += 1;
-
+    //This method is called to give 2 cards to players in start
+    public void initialHand() {
         for (Player player : players) {
+            if (player.getCards().isEmpty()) {
+                while (player.getCards().size() < 2) {
+                    player.takeCard(cards.remove(0));
 
 
-            if (!player.name.equals(winner.name)) {
-                player.loses += 1;
+                }
+            }
+
+            printHand(player);
+        }
+        checkIfWinner();
+    }
+
+
+    //This method is called to print players hand
+    public void printHand(Player player) {
+        System.out.println(player.name + "  **HAND**: ");
+        List<Card> hand = player.getCards();
+        for (Card card : hand) {
+            System.out.println(card.toString());
+
+        }
+
+    }
+
+    public boolean checkIfWinner() {
+        Player bestPlayer = null;
+        Iterator<Player> iterator = players.iterator();
+        while (iterator.hasNext()) {
+            Player player = iterator.next();
+            int currentPlayerHand = player.getValue();
+            // if player has more than 21 he loses
+            if (currentPlayerHand > 21) {
+                droppedPlayers.add(player);
+                iterator.remove();
+                System.out.println(player.name + " has more than 21 and is OUT");
+            }
+
+            // if all players are out game ends
+            if (players.isEmpty()) {
+                endOfGame();
+            }
+
+            // if player has 21 it wins
+            if (currentPlayerHand == 21) {
+                this.winner = player;
+                return true;
+            }
+            // if all players stay check who has best hand
+            if (allPlayersStayed()) {
+                if (bestPlayer == null || bestPlayer.getValue() < currentPlayerHand) {
+                    bestPlayer = player;
+                }
+                this.winner = bestPlayer;
+                return true;
+            }
+
+
+        }
+        return false;
+    }
+
+    public boolean allPlayersStayed() {
+        for (Player player : players) {
+            if (!player.stay) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public void clearHands() {
+        emptyPlayers();
+        for (Player player : droppedPlayers) {
+            player.clearHand();
+
+
+        }
+
+
+    }
+
+    private void addDroppedPlayersBack() {
+        List<Player> playersToAddBack = new ArrayList<>(droppedPlayers);
+        for (Player player : playersToAddBack) {
+            players.add(player);
+            droppedPlayers.remove(player);
+        }
+    }
+
+    public void emptyPlayers() {
+        List<Player> playersToRemove = new ArrayList<>(players);
+        for (Player player : playersToRemove) {
+            droppedPlayers.add(player);
+            players.remove(player);
+        }
+    }
+
+    //This method is called to check if game has ended if so it will ask if player wants to play again or end game
+    @Override
+    public boolean endOfGame() {
+        if (this.winner != null) {
+            this.winner.wins += 1;
+
+            for (Player player : players) {
+
+
+                if (!player.name.equals(winner.name)) {
+                    player.loses += 1;
+                }
+            }
+
+
+            //ask if player wants to play again
+            System.out.println("Do you want to play again? Y/N");
+            String answer = sc.nextLine().toUpperCase();
+            while (!answer.equals("Y") && !answer.equals("N")) {
+                System.out.println("Do you want to play again? Y/N");
+                answer = sc.nextLine().toUpperCase();
+
+            }
+
+            if (answer.equals("Y")) {
+                clearHands();
+                initializeGame(numberOfPlayers);
+                return false;
+            } else {
+                return true;
             }
         }
 
+        return false;
 
-        //ask if player wants to play again
-        System.out.println("Do you want to play again? Y/N");
-        String answer = sc.nextLine().toUpperCase();
-        while (!answer.equals("Y") && !answer.equals("N")) {
-            System.out.println("Do you want to play again? Y/N");
-            answer = sc.nextLine().toUpperCase();
 
-        }
-
-        if (answer.equals("Y")) {
-            clearHands();
-            initializeGame(numberOfPlayers);
-            return false;
-        } else {
-            return true;
-        }
     }
 
-    return false;
+    //This method is called when game ends to display winner
+    @Override
+    public void displayWinner() {
+        Player mostWins = null;
+        emptyPlayers();
 
-
-}
-
-//This method is called when game ends to display winner
-@Override
-public void displayWinner() {
-    Player mostWins = null;
-    emptyPlayers();
-
-    //find player with most wins
-    for (Player player : droppedPlayers) {
-        if (mostWins == null) {
-            mostWins = player;
+        //find player with most wins
+        for (Player player : droppedPlayers) {
+            if (mostWins == null) {
+                mostWins = player;
+            }
+            if (player.wins > mostWins.wins) {
+                mostWins = player;
+            }
         }
-        if (player.wins > mostWins.wins) {
-            mostWins = player;
+        //display winner with total wins
+        System.out.println("Winner is " + mostWins.name);
+        for (Player player : droppedPlayers) {
+            System.out.println(player.name + " had " + player.wins);
         }
-    }
-    //display winner with total wins
-    System.out.println("Winner is " + mostWins.name);
-    for (Player player : droppedPlayers) {
-        System.out.println(player.name + " had " + player.wins);
-    }
 
 
-}
+    }
 
 }
